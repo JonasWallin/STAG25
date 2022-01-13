d56721754b1388912d89e92fdad6b99adc74410e
JonasWallin <jonas.wallin81@gmail.com>
2022-01-13 20:21:33 +0100

    first lab1

---
+++ assignments/lab1/base_lab1.R
@@ -0,0 +1,63 @@
+#requires writing a script
+#handling characters
+
+
+# write three functions 
+
+
+
+#'basic arithmetic operations
+#' 
+
+#' @return 4.84log_10(51!)
+#' here log_10 is the logarithm with base 10
+#' ! stands for factorial
+artih.1 <- function(){
+  
+}
+#' @return 4.02square_root_3(7^2 + e^7)
+#' Here square_root_3 is the cubic root, and e is the euler's number
+arith.2 <- function(){
+  
+}
+
+#' @param i
+#' @return 1/1+1/2+1/3+1/4...+1/i
+#'
+arith.3 <- function(i){
+  
+}
+#' takes a vector and computes the mean value of the vector rounded at two decimals
+#' you are not allowed to use the built in function mean here
+#' @param vector.in - (n x 1) input vector
+#' @param mean.out -  (1 x 1) the mean of the vector
+Mean <- function( vector.in ){
+  
+}
+
+#'  Takes a vector and computes the variance
+#'  you are not allowed to use the built in functions mean, var, or sd here
+#'  But you can use your own Mean
+#' @param vector.in - (n x 1) input vector
+#' @return var.out -  (1 x 1) the variance of the vector
+Var <- function( vector.in){
+  
+}
+
+
+#'
+#' You should create a function that takes a vector of tw integer
+#' and return TRUE if the output is two sixes FALSE otherwise
+#' @param dices - (2 x 1)
+#' @return dices.two.sixes - (1 x 1) boolean is dices two sixes
+is.sixes <- function(dices){
+  
+}
+
+#'
+#' Generate a dice, i.e.  a uniform random number in {1,2,3,4,5,6}
+#'
+#' @return dice (int) the output
+generate.dice <- function(){
+  
+}
