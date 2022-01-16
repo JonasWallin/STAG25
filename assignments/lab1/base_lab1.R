070a6f1a4870040bd3277c4777edbe5977ec3ab3
JonasWallin <jonas.wallin81@gmail.com>
2022-01-16 23:32:50 +0100

    more tasks

--- assignments/lab1/base_lab1.R
+++ assignments/lab1/base_lab1.R
@@ -9,24 +9,32 @@
 #'basic arithmetic operations
 #' 
 
+
+# task 1
 #' @return 4.84log_10(51!)
 #' here log_10 is the logarithm with base 10
 #' ! stands for factorial
 artih.1 <- function(){
   
 }
+
+# task 2
 #' @return 4.02square_root_3(7^2 + e^7)
 #' Here square_root_3 is the cubic root, and e is the euler's number
 arith.2 <- function(){
   
 }
 
+
+# task 3
 #' @param i
 #' @return 1/1+1/2+1/3+1/4...+1/i
 #'
 arith.3 <- function(i){
   
 }
+
+# task 4
 #' takes a vector and computes the mean value of the vector
 #' you are not allowed to use the built in function mean here
 #' @param vector.in - (n x 1) input vector
@@ -35,6 +43,7 @@ Mean <- function( vector.in ){
   
 }
 
+# task 5
 #'  Takes a vector and computes the variance
 #'  you are not allowed to use the built in functions mean, var, or sd here
 #'  But you can use your own Mean
@@ -45,6 +54,8 @@ Var <- function( vector.in){
 }
 
 
+
+# task 6
 #'
 #' You should create a function that takes a vector of tw integer
 #' and return TRUE if the output is two sixes FALSE otherwise
@@ -54,6 +65,8 @@ is.sixes <- function(dices){
   
 }
 
+
+# task 7
 #'
 #' Generate a dice, i.e.  a uniform random number in {1,2,3,4,5,6}
 #'
