da8e2d164fad5b51eb4fad275099d4f1046ff1d1
JonasWallin <jonas.wallin81@gmail.com>
2022-02-04 11:42:18 +0100

    update lab1

--- assignments/lab1/task_3.R
+++ assignments/lab1/task_3.R
@@ -5,4 +5,8 @@ test_that("Marking of task 3", {
   expect_is(arith.3(1), "numeric", info = "arith.3 is not return a numeric value")
   expect_equal(arith.3(1), 1, info = "arith.3 contains erroneous values")
   expect_equal(arith.3(2), 1+1/2, info = "arith.3 contains erroneous values")
+  for(i in 1:5){
+    x <- sample(1:1000,1)
+    expect_equal(arith.3(x), sum(1/(1:x)), info = "arith.3 contains erroneous values")
+  }
 })
