d58463cbcc49e11ade675e740fe9f42d2f09a041
JonasWallin <jonas.wallin81@gmail.com>
2022-02-08 10:17:24 +0100

    bug in task_1 lab3

--- assignments/lab3/task_1.R
+++ assignments/lab3/task_1.R
@@ -5,6 +5,7 @@ context("task 1")
 test_that("Marking of task 1", {
   expect_true(exists("print.x"), info = "print.x() does not exist.")
   x <- c(1,2,3,4,5)
+  eval_printx <- evaluate_promise(print.x(x))
   output.one <- "[1] \"x[1] = 1\"\n[1] \"x[2] = 2\"\n[1] \"x[3] = 3\"\n[1] \"x[4] = 4\"\n[1] \"x[5] = 5\""
   output.two <- "x[1] = 1\nx[2] = 2\nx[3] = 3\nx[4] = 4\nx[5] = 5"
   expect_equal(TRUE, eval_printx$output%in%c(output.one,output.two),info=paste("not equal to either:\n",output.one,"\nor:\n",output.two ,sep=""))
